ca309fee1ec27ae9118c7ab9bd8eb99a51298ad6
Stanislaw Cruz <techway.stanislaw@odontosystem.com.br>
2020-05-29 23:52:59 -0300

    MVC.

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -6,6 +6,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
+const templates = require('../app/views/templates');
+
 app.use('/estatico', express.static('src/app/public'));
 
 app.use(bodyParser.urlencoded({
@@ -13,10 +15,10 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(methodOverride(function (req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      // look in urlencoded POST bodies and delete it
-      var method = req.body._method;
-      delete req.body._method;
-      return method;
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method;
+        delete req.body._method;
+        return method;
     }
 }));
 
@@ -24,15 +26,15 @@ const rotas = require('../app/rotas/rotas');
 rotas(app);
 
 app.use(function (req, resp, next) {
-  return resp.status(404).marko(
-    require('../app/views/base/erros/404.marko')
-  );
+    return resp.status(404).marko(
+        templates.base.erro404
+    );
 });
 
 app.use(function (erro, req, resp, next) {
-  return resp.status(500).marko(
-    require('../app/views/base/erros/500.marko')
-  );
+    return resp.status(500).marko(
+        templates.base.erro500
+    );
 });
 
 module.exports = app;
