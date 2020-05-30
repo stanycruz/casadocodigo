c00855f5adc920d7f151d81009288b28b0fc5d80
Stanislaw Cruz <stanycruz@outlook.com>
2020-05-30 17:47:08 -0300

    Autorização de acesso.

--- src/app/rotas/livro-rotas.js
+++ src/app/rotas/livro-rotas.js
@@ -2,9 +2,18 @@ const LivroControlador = require('../controladores/livro-controlador');
 const livroControlador = new LivroControlador();
 
 const Livro = require('../modelos/livro');
+const BaseControlador = require('../controladores/base-controlador');
 
 module.exports = (app) => {
     const rotasLivro = LivroControlador.rotas();
+
+    app.use(rotasLivro.autenticadas, function (req, resp, next) {
+        if (req.isAuthenticated()) {
+            next();
+        } else {
+            resp.redirect(BaseControlador.rotas().login);
+        }
+    });
 
     app.get(rotasLivro.lista, livroControlador.lista());
 
